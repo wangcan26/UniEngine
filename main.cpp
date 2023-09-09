35557a916405a43363f4a0bb6a0cacd08be1cc82
wangcan26 <wangcan.199@bytedance.com>
2023-09-09 22:09:50 +0800

    feat(mvp): add/test Framegraph

--- main.cpp
+++ main.cpp
@@ -3,14 +3,60 @@
 #include <memory>
 #include "app/uni_sample_app.h"
 #include "NanoLog.hpp"
+#include "fg/FrameGraph.hpp"
+#include <sstream>
+#include <fstream>
 
-using NativeSampleApp = universe::GLFWAppBase;
+using NativeSampleApp = universe::GLFWAppBase; 
+
+struct FrameGraphTexture {
+  struct Desc {
+    uint32_t width;
+    uint32_t height;
+  };
+
+  FrameGraphTexture() = default;
+  explicit FrameGraphTexture(int32_t _id) : id{_id} {}
+  FrameGraphTexture(FrameGraphTexture &&) noexcept = default;
+
+  void create(const Desc &, void *) {
+    static auto lastId = 0;
+    id = ++lastId;
+  }
+  void destroy(const Desc &, void *) {}
+
+  //void preRead(const Desc &, uint32_t flags, void *) {}
+  //void preWrite() {}
+
+  static const char *toString(const Desc &desc) { return "<I>texture</I>"; }
+
+  int32_t id{-1};
+};
 
 int main()
 {
     nanolog::initialize(nanolog::GuaranteedLogger(), "/Users/bytedance/Workspace/UniverseEngine/UniEngine/build/", "nanolog", 1);
     nanolog::set_log_level(nanolog::LogLevel::INFO);
     LOG_INFO << "Hello UniEngine";
+
+    //test FrameGraph
+    FrameGraph fg;
+
+    struct PassData {
+        FrameGraphResource target;
+    };
+    fg.addCallbackPass<PassData>("SimplePass", [&](FrameGraph::Builder &builder, PassData &data){
+        std::string_view foo{"Foo"};
+        data.target = builder.create<FrameGraphTexture>(foo, {1280, 720});
+        data.target = builder.write(data.target);
+    }, [=](const PassData &data, FrameGraphPassResources &resources, void*) {
+
+    });
+
+    fg.compile();
+    fg.execute();
+
+    std::ofstream{"fg.dot"} << fg;
     
     std::unique_ptr<NativeSampleApp> sampleApp = std::make_unique<universe::SampleApp>();
     universe::GLFWAppBase::CreateInfo createInfo {.title = "hello triangle", .minorVersion = 1, .majorVersion = 4};
@@ -24,5 +70,6 @@ int main()
     }
 
     sampleApp->deinitialize();
+
     return 0;
 }
